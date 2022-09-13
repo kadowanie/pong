ea80f4552a00b82cf8abd0abd0bd34175d53c9aa
Kindball <98301185+Kindball@users.noreply.github.com>
2022-09-13 19:54:24 +0300

    added win condition

--- ball.gd
+++ ball.gd
@@ -1,17 +1,12 @@
 extends KinematicBody2D
 
-
 var speed = 300
 var velocity = Vector2()
-
-
 func _ready():
 	start()
-
-
+	
 func start():
-	velocity = Vector2(speed, 0).rotated(rand_range(-360,360))
-
+	velocity = Vector2(speed, 0).rotated(rand_range(0,360))
 
 func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
@@ -19,10 +14,10 @@ func _physics_process(delta):
 		velocity = velocity.bounce(collision.normal)
 		if collision.collider.is_in_group("Brick"):
 			collision.collider.queue_free()
+			if get_tree().get_nodes_in_group("Brick").size()==1:
+				alert("you win!", "Victory!")
 		if collision.collider.is_in_group("floor"):
-			alert("you lose", "game over, NOOOOB!!!")
-
-
+			alert("you lose", "game over")
 func alert(text: String, title: String='Message'):
 	var dialog = AcceptDialog.new()
 	dialog.dialog_text = text
@@ -33,6 +28,3 @@ func alert(text: String, title: String='Message'):
 	get_tree().paused = true
 		#if collision.collider.has_method("hit"):
 			#collision.collider.hit()
-
-#func _on_VisibilityNotifier2D_screen_exited():
-	#queue_free()
