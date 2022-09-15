4963c7c65dbb1af7f726cacd3633bb0244726ea9
Kindball <98301185+Kindball@users.noreply.github.com>
2022-09-15 20:26:07 +0300

    Added brick class for variants
    also changed win condition checking

--- ball.gd
+++ ball.gd
@@ -1,7 +1,8 @@
 extends KinematicBody2D
 
-var speed = 300
+var speed = 600
 var velocity = Vector2()
+var damage = 1
 func _ready():
 	start()
 	
@@ -9,15 +10,18 @@ func start():
 	velocity = Vector2(speed, 0).rotated(rand_range(0,360))
 
 func _physics_process(delta):
+	check()
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		velocity = velocity.bounce(collision.normal)
 		if collision.collider.is_in_group("Brick"):
-			collision.collider.queue_free()
-			if get_tree().get_nodes_in_group("Brick").size()==1:
-				alert("you win!", "Victory!")
+			if collision.collider.has_method("hit"):
+				collision.collider.hit(damage)
+			
 		if collision.collider.is_in_group("floor"):
 			alert("you lose", "game over")
+
+
 func alert(text: String, title: String='Message'):
 	var dialog = AcceptDialog.new()
 	dialog.dialog_text = text
@@ -26,5 +30,8 @@ func alert(text: String, title: String='Message'):
 	add_child(dialog)
 	dialog.popup_centered()
 	get_tree().paused = true
-		#if collision.collider.has_method("hit"):
-			#collision.collider.hit()
+
+
+func check():
+	if get_tree().get_nodes_in_group("Brick").size()==0:
+		alert("you win!", "Victory!")
--- brick.gd
+++ brick.gd
@@ -1,16 +1,19 @@
 extends StaticBody2D
 
-
+class_name Brick
 # Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var life =1
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func hit(damage):
+	bonus()
+	life=life-damage
+	if life == 0:
+		self.queue_free()
+
+func bonus():
+	pass
